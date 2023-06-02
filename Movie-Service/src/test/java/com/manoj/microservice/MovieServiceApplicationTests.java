bbeedeb2837577ed25c0971bf1ed69efbc055779
Manoj Basnet <basnetm02@gmail.com>
2023-06-02 12:23:05 +0545

    4 microservices initial setup through spring initializr

---
+++ Movie-Service/src/test/java/com/manoj/microservice/MovieServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoj.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Rating-Service/src/test/java/com/manoj/microservice/RatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manoj.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
