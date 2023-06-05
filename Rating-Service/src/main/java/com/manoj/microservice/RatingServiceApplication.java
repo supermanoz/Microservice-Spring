1ca9113b2f6ec78c4597a2a60c01f527318d8b7c
Manoj Basnet <basnetm02@gmail.com>
2023-06-05 14:43:31 +0545

    test-dashboard not working

--- Rating-Service/src/main/java/com/manoj/microservice/RatingServiceApplication.java
+++ Rating-Service/src/main/java/com/manoj/microservice/RatingServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -15,6 +16,7 @@ import java.time.Duration;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableHystrixDashboard
 public class RatingServiceApplication {
 
 	public static void main(String[] args) {
